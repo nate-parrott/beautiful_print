81845d634153d5eee703a62f1e35bb9a18414b00
nate-parrott <nateparro2t@gmail.com>
2014-09-01 18:41:10 -0400

    fix dependency

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'beautiful_print',
   packages = ['beautiful_print'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.11',
   description = 'Prettier pretty-printing of HTML.',
   author = 'Nate Parrott',
   author_email = 'nate@nateparrott.com',
@@ -10,5 +10,5 @@ setup(
   download_url = 'https://github.com/nate-parrott/beautiful_print/tarball/0.1', # I'll explain this in a second
   keywords = ['HTML', 'BeautifulSoup', 'pretty print', 'readable'], # arbitrary keywords
   classifiers = [],
-	install_requires = ['bs4']
+	install_requires = ['beautifulsoup4']
 )
